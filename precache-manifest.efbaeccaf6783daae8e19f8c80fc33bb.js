49aa44a957f84cf9adffa5356b84373db8a4b8e2
abhisanGhimire <abhisanghimire@dr-ait.org>
2020-08-26 14:54:59 +0545

    Updates

---
+++ precache-manifest.efbaeccaf6783daae8e19f8c80fc33bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf7f7c0ca8e9aa17d51350fb42170ea",
+    "url": "/invictus_react/index.html"
+  },
+  {
+    "revision": "7c090bb983ffc5eca175",
+    "url": "/invictus_react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "32a0c716de10f558ba01",
+    "url": "/invictus_react/static/css/main.02528dba.chunk.css"
+  },
+  {
+    "revision": "7c090bb983ffc5eca175",
+    "url": "/invictus_react/static/js/2.28d30c3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/invictus_react/static/js/2.28d30c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a0c716de10f558ba01",
+    "url": "/invictus_react/static/js/main.bc69289d.chunk.js"
+  },
+  {
+    "revision": "04b65339501b0e0767b8",
+    "url": "/invictus_react/static/js/runtime-main.045842cf.js"
+  }
+]);
